1a1d83b622a96043478c4c579d8ed1688826b22f
LuisMontenegro21 <83621697+LuisMontenegro21@users.noreply.github.com>
2025-02-08 00:52:50 -0600

    Working on functions

--- src/TuringMachineEmulator.py
+++ src/TuringMachineEmulator.py
@@ -1,21 +1,49 @@
 import yaml
+import numpy as np
 
 def read_yaml(file_path: str):
-    with open(file_path, 'r') as stream:
+    '''
+    reads yaml input and returns the instructions 
+    params: file_path str
+    '''
+    with open(file_path, 'r') as stream: # load the path 
         try:
-            instructions = yaml.safe_load(stream=stream)
+            instructions = yaml.safe_load(stream=stream) # load the tokens
             return instructions
         except yaml.YAMLError as exception:
             raise exception
         
-def convert_to_binary(x: int):
+def convert_to_binary(n: int) -> np.ndarray:
+    '''
+    converts an integer to binary and returns it as np.ndarray
+    '''
+    if not isinstance(n, int): # in case no int arg was passed
+        raise TypeError(f"TypeError: {n} must be type 'int', {type(n)} found instead")
+
+    binary: str = format(n, 'b') # transform to binary str
+    return np.array(list(binary)) # return a np.ndarray of the binary
+
+
+# TODO not necesary
+def add_tape_length(tape: np.ndarray, direction: str):
     pass
 
-def add_tape_length(tape: list, direction: str):
+def traverse_tape(n: int, binary_num: np.ndarray, position: int, instructions: dict) -> None:
+    halt_state = instructions['q_states']['final'] # get the final state
+
+
+def iterate(delta: dict , tape: np.ndarray, initial_poisition: int):
     pass
 
-def traverse_tape():
+
+def is_accepted(n: int, binary_num: np.ndarray, final_state: int, halt_state: int):
     pass
 
-def iterate(delta, tape, initial_poisition):
-    pass
+def print_instant_production(state: int, tape: np.ndarray, position: int):
+    '''
+    print instant productions 
+    '''
+    tape: np.ndarray = np.array(['B' if symbol is None else symbol for symbol in tape]) # replace None instances for Blank
+    tape[position] = f'[{state}, {tape[position]}]' # change the current position for the state and position it is
+    tape_content: str = ''.join(str(symbol) for symbol in tape) # join the symbols found in the current array
+    print(f"\t{tape_content}") # print content currently in the tape
--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from TuringMachineEmulator import *
-
-
+import sys
 
 def main():
-    n: int = int(input("Enter n-Fibonacci to calculate: "))
+    file_path = str(sys.argv[1]) if len(sys.argv) < 3 else None
+    n: int = int(input("\nEnter n-Fibonacci to calculate: "))
+    binary_num : np.ndarray = convert_to_binary(n=n) # convert to binary equivalence as np.array
+    stream: dict = read_yaml(file_path=file_path)
+    
     
 if __name__ == '__main__':
     main()
